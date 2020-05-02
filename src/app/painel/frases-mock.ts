19bb530e4ea89483659343eb84184a988ceeb639
Wilkson Carlos <wilkson.carlos@outlook.com>
2020-05-02 17:29:19 -0300

    Atualizando painel atualizar barra progresso

--- src/app/painel/frases-mock.ts
+++ src/app/painel/frases-mock.ts
@@ -1,13 +1,10 @@
 import { Frases } from "../shared/frase.model";
 
 export const FRASES: Frases[] =[
-    { fraseEng:'I like you', frasePtBr:'Eu gosto de você' },
     { fraseEng:'I like to learn', frasePtBr:'Eu gosto de aprender' },
-    { fraseEng:'I watch tv', frasePtBr:'Eu assito tv' },
+    { fraseEng:'I watch tv', frasePtBr:'Eu assisto tv' },
     { fraseEng:'I love eat', frasePtBr:'Eu amo comer' },
     { fraseEng:'How are you?', frasePtBr:'Como vai você?' },
-    { fraseEng:'I eat bread', frasePtBr:'Eu como pão' },
-    { fraseEng:'How old are you?', frasePtBr:'Quantos anos você tem?' },
-    { fraseEng:'Books are amazing', frasePtBr:'livros são incríveis' }
+    //{ fraseEng:'I eat bread', frasePtBr:'Eu como pão' },
 
 ]
--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -11,17 +11,41 @@ export class PainelComponent implements OnInit {
 
   public frases: Frases[] = FRASES
   public instrucao: string = "Traduza a frase: "
-  public resposta: string
+  public resposta: string = ''
 
-  constructor() {console.log(this.frases);
+  public rodada: number = 0
+  public rodadaFrase: Frases
+
+  public progresso: number = 0 
+
+  constructor() {
+    this.atualizaRodada()
   }
 
   ngOnInit() {
   }
   public atualizaResposta(resposta: Event): void {
-    this.resposta = (<HTMLInputElement>resposta.target).value
-    console.log(this.resposta);
+    this.resposta = (<HTMLInputElement>resposta.target).value   
+  }
+
+  public verificarResposta(): void{
+    if (this.rodadaFrase.frasePtBr == this.resposta){
+      alert("Parabéns! sua resposta está correta!")
+      this.rodada++
+      this.progresso = this.progresso + (100 / this.frases.length)
+      this.atualizaRodada()    
+      
+    }
+    else{
+      alert("Poxa sua resposta está errada, mas não desista continue tentando!! :)")
+    }
     
+  }
+  public atualizaRodada(): void{
+    this.rodadaFrase = this.frases[this.rodada]
+
+    this.resposta = ''
+
   }
 
 }
